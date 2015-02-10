73979aad2f6231adc833edb0f0b4061991896e70
Romain Crestey <rcrestey@gmail.com>
2015-02-10 19:58:38 +0100

    changed createRay.fromMouse parameter

--- utils/ray/createRay.js
+++ utils/ray/createRay.js
@@ -4,22 +4,17 @@ var THREE = require('three');
 
 module.exports = function(camera){
 
-    function fromMouse(x, y){
+    function fromMouse(mousePosition){
         console.log('creating ray from mouse');
         // event ca be a mousemove or click
         var projector = new THREE.Projector();
-
-        var mouse = {
-            x: 0,
-            y: 0
-        };
-
+        
         // Get the mouse X and Y screen positions, and scale them to [-1, 1] ranges, position (-1, 1) being the upper left side of the screen.
-        mouse.x = (x / window.innerWidth) * 2 - 1;
-        mouse.y = - (y / window.innerHeight) * 2 + 1;
+        var x = (mousePosition.x / window.innerWidth) * 2 - 1;
+        var y = - (mousePosition.y / window.innerHeight) * 2 + 1;
         
         // Create Vector3 from mouse position, with Z = 0
-        var mousePos = new THREE.Vector3(mouse.x, mouse.y, 0);
+        var mousePos = new THREE.Vector3(x, y, 0);
 
         // Create a picking-specific RayCaster from Three.js library 
         return projector.pickingRay(mousePos, camera);
